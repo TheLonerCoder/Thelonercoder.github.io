696226c8091babbdefb028b0355d2f17c3344e5d
TheLonerCoder <thecodingloner@gmail.com>
2022-05-24 17:59:02 -0500

    Facelift with Darkmode
    Alot of microchanges with a basic markmode feature..

---
+++ index.js
@@ -0,0 +1,17 @@
+function changeBackground() {
+
+    if (document.body.style.background == `rgb(30, 30, 30)`) {
+
+        document.body.style.background = 'white';
+        document.body.style.color = 'black';
+        // document.getElementById(`portHeading`).style.backgroundColor = 'rgba("$color: #2d9bf5", "$alpha: 1.0")';
+
+    } else {
+
+        document.body.style.background = '#1E1E1E';
+        document.body.style.color = 'white';
+        document.body.a.style.color = '';
+        // document.getElementById(`portHeading`).style.backgroundColor = 'grey';
+
+    }
+}
